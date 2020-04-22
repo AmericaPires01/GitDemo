2f1647f03acd611d02e5fdf2187e99533800b5a1
AmericaPires01 <americapires4@gmail.com>
2020-04-22 14:01:50 +0100

    This is commit from the user who cloned GitDemo

--- src/test/java/step/definitions/LogInSteps.java
+++ src/test/java/step/definitions/LogInSteps.java
@@ -8,19 +8,25 @@ import io.cucumber.java.en.When;
 public class LogInSteps {
     @Given("^User is on NetBanking landing page$")
     public void userIsOnNetBankingLandingPage() {
+
+        System.out.println("-----Person who has downloaded GitDemo------ ");
     }
 
-    @When("^User log into the application with user name and password$")
+    @When("User log into the application with user name and password")
     public void userLogIntoTheApplicationWithUserNameAndPassword() {
 
+        System.out.println("-----123456778Person who downloaded GitDemo------ ");
+
     }
 
-    @Then("^Home page is populated$")
+    @Then("Home page is populated")
     public void homePageIsPopulated() {
 
+        System.out.println("-----Love love more love Person who downloaded GitDemo------ ");
+
     }
 
-    @And("^Cards are displayed$")
+    @And("Cards are displayed")
     public void cardsAreDisplayed() {
     }
 }
