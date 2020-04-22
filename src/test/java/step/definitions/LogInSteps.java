e4b4ed137e645c134ca2a787a1ac4fdb1f5d88e9
AmericaPires01 <americapires4@gmail.com>
2020-04-22 14:34:01 +0100

    GitStuff changes

--- src/test/java/step/definitions/LogInSteps.java
+++ src/test/java/step/definitions/LogInSteps.java
@@ -9,20 +9,20 @@ public class LogInSteps {
     @Given("^User is on NetBanking landing page$")
     public void userIsOnNetBankingLandingPage() {
 
-        System.out.println("-----Person who has downloaded GitDemo------ ");
+        System.out.println("-----GitStuff ------ ");
     }
 
     @When("User log into the application with user name and password")
     public void userLogIntoTheApplicationWithUserNameAndPassword() {
 
-        System.out.println("-----123456778Person who downloaded GitDemo------ ");
+        System.out.println("-----123 GitStaff------ ");
 
     }
 
     @Then("Home page is populated")
     public void homePageIsPopulated() {
 
-        System.out.println("-----Love love more love Person who downloaded GitDemo------ ");
+        System.out.println("-----Test 123...------ ");
 
     }
 
